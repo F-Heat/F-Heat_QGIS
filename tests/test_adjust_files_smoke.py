b98185d760373cdd2eab0fa095726e17a6efa7ef
Philipp Sommer <95475743+psomm@users.noreply.github.com>
2025-08-22 16:46:51 +0200

    Refactor tests: simplify assertions and remove unused test cases

--- tests/test_adjust_files_smoke.py
+++ tests/test_adjust_files_smoke.py
@@ -1,11 +1,11 @@
 import sys
 from pathlib import Path
 import pandas as pd
+import math
 
 from adjust_files import Buildings_adj
 
 
 def test_extract_year():
     assert Buildings_adj.extract_year('2023-07-17') == 2023
-    import numpy as np
-    assert (Buildings_adj.extract_year(None) != Buildings_adj.extract_year(None))  # returns nan
+    assert math.isnan(Buildings_adj.extract_year(None))  # returns nan
